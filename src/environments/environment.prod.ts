3778041b9ea256fc38103eeb09d7eb10942772d1
techmonks-learn <techmonks-learn@gmail.com>
2020-07-20 22:51:05 +0530

    removed unused values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 declare var apiConfig: any;
 export const environment = {
   production: true,
-  apiUrl: 'https://rdted.technosoftcorp.net/api',
+  apiUrl: 'https://tedapi.technosoftcorp.net/',
   productsApiSuffix:'products-api',
   cartAPISuffix:'cart-api',
-  encryptionKey:"",
   baseHref: document.getElementsByTagName('base')[0].href,
+  encryptionKey:'',
   cdnURL:'',
-  apiGatewayUrl:'https://tedapi.technosoftcorp.net/usr-api',
+  apiGatewayUrl:'https://tedapi.technosoftcorp.net/',
   codeQualityUrl:'https://tedapi.technosoftcorp.net/usr-api/'
 };
  // apiGatewayUrl:'https://rdted.technosoftcorp.net/api',

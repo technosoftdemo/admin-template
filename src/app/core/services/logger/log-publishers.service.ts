121968b1fd953dddc51a2e375743a5f8880ef10d
techmonks-learn <techmonks-learn@gmail.com>
2020-08-19 20:41:36 +0530

    Made "Logger changes" (Exclude LogWebAPI on server side)

--- src/app/core/services/logger/log-publishers.service.ts
+++ src/app/core/services/logger/log-publishers.service.ts
@@ -1,13 +1,15 @@
-﻿import { Injectable } from '@angular/core';
+﻿import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { LogPublisher, LogConsole, LogLocalStorage, LogWebApi } from "./log-publishers";
 import { ApiClient } from '../api-client.service';
+import { isPlatformServer } from '@angular/common';
 
 // ****************************************************
 // Logging Publishers Service Class
 // ****************************************************
-@Injectable({providedIn:'root'})
+@Injectable({ providedIn: 'root' })
 export class LogPublishersService {
-  constructor(private _apiClient: ApiClient) {
+  constructor(private _apiClient: ApiClient,
+    @Inject(PLATFORM_ID) private platformId) {
     // Build publishers arrays
     this.buildPublishers();
   }
@@ -25,8 +27,10 @@ export class LogPublishersService {
 
     // Create instance of LogLocalStorage Class
     // this.publishers.push(new LogLocalStorage());
+    if (isPlatformServer(this.platformId) === false) {
+      //Create instance of LogWebApi Class
+      this.publishers.push(new LogWebApi(this._apiClient));
+    }
 
-    //Create instance of LogWebApi Class
-    // this.publishers.push(new LogWebApi(this._apiClient));
   }
 }
